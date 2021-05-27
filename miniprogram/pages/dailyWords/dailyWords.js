dac7af80c328aaa49d1922096908f14ee455a687
aaron <chunjianr@gmail.com>
2021-05-27 17:40:58 +0800

    fix: 导航框设置圆角，上传晕圈图片；

--- miniprogram/pages/dailyWords/dailyWords.js
+++ miniprogram/pages/dailyWords/dailyWords.js
@@ -140,27 +140,6 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    console.log('daliyWords hide.')
-  },
-
-  /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
